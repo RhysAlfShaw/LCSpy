c8950379280e3fcdc695ba18b326a714023d4d67
Rhys Shaw <rhysc3p0@gmail.com>
2023-02-03 10:26:55 +0000

    Update

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import src.LoFAR_cutout as LoFAR_cutout
+import src.LCSpy as LCSpy
 import unittest
 
 class TestArithmetic(unittest.TestCase):
 
     def test_cutout_cat(self):
-        LoFAR_cutout.cutout_2d(190,55) 
+        LCSpy.cutout_2d(190,55) 
 
 if __name__ == '__main__':
     unittest.main()
